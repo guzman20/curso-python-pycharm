f18163f41826ac4ef9042b523ba2c693394f9d24
guzman20 <guzmanaf00@gmail.com>
2022-11-24 09:13:48 +0100

    Ejercicio colegio aniadida confeguracion de lectura de ficheros

--- colegio/main.py
+++ colegio/main.py
@@ -1,6 +1,16 @@
 from Classes.alumno import Alumno
 from Classes.colegio import Colegio
 
+# Parametros de lectura del fichero
+COLEGIO = 0
+ALUMNO_NOMBRE = 1
+ALUMNO_APELLIDOS = 2
+ALUMNO_DNI = 3
+ALUMNO_ASIGNATURAS = 4
+SEPARADOR_ALUMNOS = "|"
+SEPARADOR_ASIGNATURAS = ";"
+
+
 if __name__ == '__main__':
     colegios = {}
     try:
@@ -9,16 +19,16 @@ if __name__ == '__main__':
         # Recorremos las lineas del archivo txt
         for linea in archivo:
             # Dividimos la lineas por las | que separan los elementos
-            palabras = linea.split("|")
+            palabras = linea.split(SEPARADOR_ALUMNOS)
             # Sacamos el nombre del colegio
-            colegio = palabras[0]
+            colegio = palabras[COLEGIO]
             # Si el colegio ya se encuentra entre las claves del diccionario solo hay que añadir al alumno
             if colegio in colegios.keys():
                 # Extraemos el colegio
                 colegioSeleccionado = colegios[colegio]
                 # Creamos el alumno a partir de la informacion que nos dan
-                alumno = Alumno(palabras[1], palabras[2], palabras[3])
-                asignaturas = palabras[4].split(";")
+                alumno = Alumno(palabras[ALUMNO_NOMBRE], palabras[ALUMNO_APELLIDOS], palabras[ALUMNO_DNI])
+                asignaturas = palabras[ALUMNO_ASIGNATURAS].split(SEPARADOR_ASIGNATURAS)
                 for asignatura in asignaturas:
                     alumno.introducir_asignatura(asignatura)
                 # Añadimos el alumno al colegio con la clave de su dni
@@ -31,8 +41,8 @@ if __name__ == '__main__':
                 colegios[colegio] = Colegio(colegio)
                 colegioSeleccionado = colegios[colegio]
                 # El resto es como antes
-                alumno = Alumno(palabras[1], palabras[2], palabras[3])
-                asignaturas = palabras[4].split(";")
+                alumno = Alumno(palabras[ALUMNO_NOMBRE], palabras[ALUMNO_APELLIDOS], palabras[ALUMNO_DNI])
+                asignaturas = palabras[ALUMNO_ASIGNATURAS].split(SEPARADOR_ASIGNATURAS)
                 for asignatura in asignaturas:
                     alumno.introducir_asignatura(asignatura)
                 colegioSeleccionado.introducir_alumno(alumno.dni, alumno)
@@ -46,18 +56,25 @@ if __name__ == '__main__':
         # Importante cerramos el archivo incluso si da error de lectura
         archivo.close()
     try:
+        # Creamos el archivo de escritura
         archivo = open("prueba2.txt", 'w')
+        # Recorremos los colegios
         for colegio in colegios.values():
+            # Por alumno
             for alumno in colegio.lista_alumnos().values():
-                string = colegio.nombre + "|" + alumno.nombre + "|" + alumno.apellidos + "|" + alumno.dni + "|"
+                # Creamos la linea del alumno con su informacion dividida por tuberias
+                string = colegio.nombre + SEPARADOR_ALUMNOS + alumno.nombre + SEPARADOR_ALUMNOS + alumno.apellidos + SEPARADOR_ALUMNOS + alumno.dni + SEPARADOR_ALUMNOS
+                # Hacemos la lista de asignaturas divididas por el punto y coma
                 for asignatura in alumno.notas():
                     if alumno.notas()[-1] != asignatura:
-                        string = string + asignatura + ";"
+                        string = string + asignatura + SEPARADOR_ASIGNATURAS
                     else:
                         string = string + asignatura
+                # Ecribimos la linea ,es decir, el alumno
                 archivo.write(string)
     except IOError:
         print("Error")
     finally:
+        # Importante cerramos el archivo incluso si da error de lectura
         archivo.close()
 
